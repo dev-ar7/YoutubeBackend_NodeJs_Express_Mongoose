5b6c1b8693490a3f4967be9602c0bb74b65450be
Arnab <arnab.ar7@gmail.com>
2024-01-11 21:59:10 +0530

    MongoDB-Atlas Connection with mongoose

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import { DB_NAME } from '../constants.js';
+
+
+const connectDB = async () => { 
+    try {
+        const connectionInstance = await mongoose.connect(
+                `${process.env.MONGODB_URI}/${DB_NAME}`
+            );
+            console.log(`\n MONGODB Connected Successfully... !! DB HOST : ${connectionInstance}`);
+    } catch (error) {
+        console.log("MONGODB failed to connect !!!", error);
+        process.exit(1);
+    }
+ }
+
+ export default connectDB;
