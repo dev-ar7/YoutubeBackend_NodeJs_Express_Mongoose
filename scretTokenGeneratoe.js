5ddb21b4205dd81b4875bae00c5b73bda168a6a3
Arnab <arnab.ar7@gmail.com>
2024-01-13 00:27:22 +0530

    Added video.models and added bcrypt, jwt and mongoose-aggregate-paginate methods and configs

---
+++ scretTokenGeneratoe.js
@@ -0,0 +1,11 @@
+import crypto from 'crypto';
+
+// Function to generate a random hex token
+const generateToken = (length = 32) => {
+  return crypto.randomBytes(length).toString('hex');
+};
+
+// Generate a token
+const secretToken = generateToken();
+
+console.log('Generated Token:', secretToken);
