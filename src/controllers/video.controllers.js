d10cf4ef22f923ee67ccbcb2730cce516801e6fd
Arnab <arnab.ar7@gmail.com>
2024-01-18 23:35:32 +0530

    Changes in Video Controller

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -6,20 +6,38 @@ import {ApiResponseHandler} from '../utils/ApiResponseHandler.js';
 import { Video } from '../models/video.models.js';
 
 
-const getAllVideos = asyncHandlerUsingPromise(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-})
+const getAllVideos = asyncHandlerUsingPromise(
+    async (req, res) => {
+        const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+        //TODO: get all videos based on query, sort, pagination
+    }
+)
 
 const publishAVideo = asyncHandlerUsingPromise(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
 })
 
-const getVideoById = asyncHandlerUsingPromise(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+const getVideoById = asyncHandlerUsingPromise(
+    async (req, res) => {
+        const { videoId } = req.params
+        //TODO: get video by id
+        if (!videoId) {
+            throw new ApiErrorHandler(404, "Sorry! No Video Found.")
+        }
+
+        const video = await Video.findById(videoId);
+
+        res.status(200)
+            .json(
+                new ApiResponseHandler(
+                    200,
+                    video,
+                    "Video fetched successfully."
+                )
+            )
+    }
+)
 
 const updateVideo = asyncHandlerUsingPromise(async (req, res) => {
     const { videoId } = req.params
