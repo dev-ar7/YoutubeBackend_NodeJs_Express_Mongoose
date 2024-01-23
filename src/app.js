cb75c47200a34935389826e30581849521ba30bf
Arnab <arnab.ar7@gmail.com>
2024-01-23 23:21:42 +0530

    Added comment routes in app-js and fixed a small bug in comment-routes

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ import userRouter from './routes/user.routes.js';
 import videoRouter from "./routes/video.routes.js";
 import postRouter from "./routes/post.routes.js";
 import healthcheckRouter from "./routes/apiHealthcheck.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 // import subscriptionRouter from "./routes/subscription.routes.js"
-// import commentRouter from "./routes/comment.routes.js"
 // import likeRouter from "./routes/like.routes.js"
 // import playlistRouter from "./routes/playlist.routes.js"
 // import dashboardRouter from "./routes/dashboard.routes.js"
@@ -31,8 +31,9 @@ app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/comments", commentRouter)
+
 // app.use("/api/v1/subscriptions", subscriptionRouter)
-// app.use("/api/v1/comments", commentRouter)
 // app.use("/api/v1/likes", likeRouter)
 // app.use("/api/v1/playlist", playlistRouter)
 // app.use("/api/v1/dashboard", dashboardRouter)
