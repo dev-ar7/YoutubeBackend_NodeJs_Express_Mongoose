069a5dd5780c214400e71a10767708cee468bd51
Arnab <arnab.ar7@gmail.com>
2024-01-27 00:29:21 +0530

    Created dashboard-controller, dashboard-routes and added in app-js

--- src/app.js
+++ src/app.js
@@ -24,10 +24,11 @@ import commentRouter from "./routes/comment.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
-// import dashboardRouter from "./routes/dashboard.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/posts", postRouter);
@@ -35,8 +36,6 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlist", playlistRouter)
-// app.use("/api/v1/dashboard", dashboardRouter)
 
-// http://localhost:8000/api/v1/users/register
 
 export { app }
--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -1,17 +1,91 @@
 import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import {Video} from "../models/video.models.js";
+import {Subscription} from "../models/subscription.models.js";
+import {Like} from "../models/like.models.js";
+import {ApiErrorHandler} from "../utils/ApiErrorHandler.js"
+import {ApiResponseHandler} from "../utils/ApiResponseHandler.js"
 import {asyncHandlerUsingPromise} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandlerUsingPromise(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    try {
+        
+        const userId = req.user._id
+
+        if (!userId) {
+            throw new ApiErrorHandler(400, "Invalid Request")
+        }
+
+        const totalVideoViews = await Video.aggregate([
+            {
+                $match: {
+                    user: mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalViwes: {
+                        $sum: "$views"
+                    }
+                }
+            }
+        ])
+
+        const totalSubscribers = await Subscription.countDocuments(
+            {channel: userId}
+        )
+        
+        const totalVideos = await Video.countDocuments(
+            {user: userId}
+        )
+
+        const totalLikes = await Like.countDocuments(
+            {video: {
+                $in: await Video.find(
+                    {user: userId}, "_id"
+                )
+            }}
+        );
+
+        const channelStats = {
+            totalVideoViews: totalVideoViews.length > 0 ? totalVideoViews[0].totalViwes : 0,
+            totalSubscribers,
+            totalVideos,
+            totalLikes
+        }
+
+        return res.status(200)
+            .json(
+                new ApiResponseHandler(
+                    200,
+                    channelStats,
+                    "Your channel stats fetched successfully."
+                )
+            )
+
+    } catch (error) {
+        throw new ApiErrorHandler(400, error.message)
+    }
 })
 
 const getChannelVideos = asyncHandlerUsingPromise(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    try {
+        const userId = req.user._id
+
+        const channelVideos = await Video.find({user: userId})
+
+        return res.status(200)
+            .json(
+                new ApiResponseHandler(
+                    200,
+                    channelVideos,
+                    "All the videos of your channel added successfully."
+                )
+            )
+
+    } catch (error) {
+        throw new ApiErrorHandler(400, error.message)
+    }
 })
 
 export {
